80cdec2f0fab1cf1b6e50919902519f32f51e23f
Rafa Adorna <rafael.adorna@protonmail.ch>
2024-09-21 23:00:07 +0000

    Make AMI optional and autoselect latest if empty values.

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,40 @@
+data "aws_ami" "ec2_instance" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "description"
+    values = ["Amazon Linux 2 *"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 resource "aws_instance" "ec2_instance" {
-  ami             = var.ami
+  ami             = coalesce(var.ami, data.aws_ami.ec2_instance.id)
   instance_type   = "t2.micro"
   key_name        = var.key_name
   subnet_id       = var.subnet_id
